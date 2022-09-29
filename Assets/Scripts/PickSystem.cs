53084ffd80d0a9d46a70789f0323e63f72ee8e02
yorka123 <york8662067@gmail.com>
2022-09-30 01:27:17 +0800

    9/30
    bug bugg
    有辦法在hitbox內判定了
    但要按很多下才有機會成功。

--- Assets/Scripts/PickSystem.cs
+++ Assets/Scripts/PickSystem.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class PickSystem : MonoBehaviour
 {
+    bool Pick = false;
+    GameObject PickObject; // 被撿取物件
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Collectable"))
         {
             if (Input.GetButtonDown("Submit")) // F or Enter
             {
-                Debug.Log("You have collected an item!");
-                
-                Destroy(collision.gameObject);
-                // 然後放入物品欄之類的
+                PickObject = collision.gameObject;
+                Pick = true; // 改：為了讓判定不會被重置(但好像沒用)
+                // ERROR 現在變成在範圍中偶爾判定一次成功
             }
-            // ERROR OnTriggerStay2D沒辦法持續偵測物件
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Pick)
+        {
+            Debug.Log("You have collected an item!");
+            
+            Destroy(PickObject);
+            
+            Pick = false;
+            // 然後放入物品欄之類的
         }
     }
 }
