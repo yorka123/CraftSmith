1a524b56880f794cef5caacd6645214833e7dc48
yorka123 <york8662067@gmail.com>
2022-10-13 13:08:03 +0800

    10/13
    背包

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData
+{
+    public Sprite Icon = null;
+    public int m_Count = 0;
+    public int m_ID = 0;
+    public bool m_Equip = false;
+}
