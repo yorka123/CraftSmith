2b623b8ec9e8b75afc638aa59ead4b1a54deee01
yorka123 <york8662067@gmail.com>
2022-10-18 01:45:12 +0800

    10/18
    F**k me.
    
    待修復物件：
    - 血條(不知為何材質跑掉了)
    - 背包系統(UI直接消失，需要重建置)
    兩個都還在開發中。
    反正下次要記得存檔！！！

--- Assets/Scripts/BagUnit.cs
+++ Assets/Scripts/BagUnit.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BagUnit : MonoBehaviour
 {
 
+    public Image m_Icon = null;
+    public Text m_CountText = null;
+    public Text m_EquipText = null;
 
+    public ItemData m_TempData = null;
+
+    public void Refresh(ItemData itemData)
+    {
+        m_TempData = itemData;
+        m_Icon.sprite = itemData.Icon;
+        m_CountText.text = itemData.m_Count.ToString();
+        m_EquipText.text = "";
+    }
 
 }
