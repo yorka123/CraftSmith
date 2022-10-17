ab615ef07947a4f9b40397bfa6753c49d75f6e9f
yorka123 <york8662067@gmail.com>
2022-10-18 01:28:56 +0800

    Revert "Bag"
    This reverts commit b6230e85afa782b8d3f069eab9b6f461cc783a65.

--- Assets/Scripts/BagManager.cs
+++ Assets/Scripts/BagManager.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 public class BagManager : MonoBehaviour
 {
 
-    public BagUnit m_CloneUnit = null;
-    public Transform m_BageNode = null;
 
-    public Sprite[] mTotalItemSprite = null;
-
-    private void Awake()
-    {
-        
-    }
 
 }
