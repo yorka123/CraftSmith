de9cc236495875c2defcaa68be1159fc0c1f92a9
yorka123 <york8662067@gmail.com>
2022-11-07 12:17:05 +0800

    Bag(1.15/2)
    Inventory.cs改隨SlotManager子物件數量填入isFull& slots.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,6 +7,15 @@ public class Inventory : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
+    public GameObject SlotManager;
 
 
+    private void Start()
+    {
+        for (int i = 0; i < SlotManager.transform.childCount; i++) // childCount: 取得子物件數量
+        {
+            slots[i] = SlotManager.transform.GetChild(i).gameObject;
+        }
+        // 添加isFull內bool數
+    }
 }
