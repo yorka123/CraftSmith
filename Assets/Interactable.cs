1b363d20469f8ff4cfb92cf739b4920b7eacb98f
yorka123 <york8662067@gmail.com>
2022-11-18 00:59:04 +0800

    11/18
    https://www.youtube.com/watch?v=SWXwfx-14lU&ab_channel=Chris%27Tutorials

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -6,10 +6,16 @@ public class Interactable : MonoBehaviour
 {
     // 待增：判定範圍(取代collider-trigger?)
 
-    public float radius = 3f;
+    public float radius = 0.01f;
     public Transform interactionTransform;
 
     bool hasIteracted = false;
+
+    private void OnDrawGizmosSelected() // Gizmos：輔助線
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
 
     public virtual void Interact()
     {
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    private void Update()
+    {
+
+
+        if (Input.GetKeyDown("Submit"))
+        {
+            
+            // ¨Ï¥ÎCollider.cast
+
+        }
+    }
+
+}
