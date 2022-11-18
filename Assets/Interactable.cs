e383fb5450cfce60262c7c2fa565a7d419a5066e
yorka123 <york8662067@gmail.com>
2022-11-19 02:34:54 +0800

    11/19

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    // 待增：判定範圍(取代collider-trigger?)
+    // 待增：判定範圍(取代collider-trigger?) (NO
 
     public float radius = 0.01f;
     public Transform interactionTransform;
 
     bool hasIteracted = false;
 
-    private void OnDrawGizmosSelected() // Gizmos：輔助線
+    private void OnDrawGizmosSelected() // Gizmos：輔助線 (+ https://dotblogs.com.tw/coolgamedevnote/2018/03/04/211220
+
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, radius);
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Transform PlayerTransform;
+    public Collider2D PlayerCollider;
 
     private void Update()
     {
